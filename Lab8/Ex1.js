793c6e9cebb782df5eedf0b1fe728500c6e5d0e6
noahkimAC <kimnoah@hawaii.edu>
2021-02-23 18:11:00 -1000

    Lab8

---
+++ Lab8/Ex1.js
@@ -0,0 +1,10 @@
+require("./product_data.js"); 
+
+var num_products = 5;
+
+var item_num = 1;
+
+while(item_num != (num_products +1)) {
+    console.log(item_num)
+    item_num++;
+}
---
+++ Lab8/product_data.js
@@ -0,0 +1,21 @@
+name1 = "HTC";
+ price1 = 40.00;
+ image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
+
+ name2 = "Apple";
+ price2 = 75.00;
+ image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
+
+ name3 = "Nokia";
+ price3 = 35.00;
+ image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
+
+ name4 = "Samsung";
+ price4 = 45.00;
+ image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
+
+ name5 = "Blackberry";
+ price5 = 10.00;
+ image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
+
+console.log("Product variables loaded...");
