8fafebb1ae1cb5bbb314efee9e4ad3fa4eb98669
noahkimAC <kimnoah@hawaii.edu>
2021-02-25 18:00:43 -1000

    Lab8

--- Lab8/Ex2.js
+++ Lab8/Ex2.js
@@ -2,10 +2,7 @@ require("./products_data.js");
 
 var num_products = 5;
 
-var item_num = 1;
-
-while(item_num <= (num_products/2)) {
+for(var item_num = 1; eval("typeof name"+item_num) != 'undefined'; item_num++) {
     console.log(`${item_num}. ${eval('name' + item_num)}`);
-    item_num++;
 }
-console.log("That's all we have!")
+console.log("That's all we have!");
