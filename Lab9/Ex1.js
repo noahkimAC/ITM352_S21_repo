1e56467daec145097472566348329539429c95e3
noahkimAC <kimnoah@hawaii.edu>
2021-03-10 02:36:13 -1000

    Lab9

--- Lab9/Ex1.js
+++ Lab9/Ex1.js
@@ -1,16 +1,18 @@
-m=11;
-d=30;
-y=2000;
+m =1 1;
+d = 30;
+y = 2000;
 ys = String(y);
 Number(ys[2]+ys[3]);
 ys[0];
-step1=0;
+step1 = 0;
 step1/4;
 step2 = 0;
 step3 = 00 + 0;
-Math.floor(step1/4); // To keep integer or parseInt(step1/4)
+// Optional Step bc my last two digits are 0 
+// Math.floor(step1/4); or parseInt(step1/4); (To keep integer)
 step4 = 5;
 step6 = step4 + step3;
 step7 = step6 + d;
-step8 = step7- 1;
-step8/7;
+step8 = step7 - 1;
+parseInt(step8/7);
+console.log(step8)
