faca9ba4e5ef95e9c05b34981e5b233ddc03c8ff
noahkimAC <kimnoah@hawaii.edu>
2021-03-09 16:47:56 -1000

    Lab9

---
+++ Lab9/Ex1.js
@@ -0,0 +1,16 @@
+m=11;
+d=30;
+y=2000;
+ys = String(y);
+Number(ys[2]+ys[3]);
+ys[0];
+step1=0;
+step1/4;
+step2 = 0;
+step3 = 00 + 0;
+Math.floor(step1/4); // To keep integer or parseInt(step1/4)
+step4 = 5;
+step6 = step4 + step3;
+step7 = step6 + d;
+step8 = step7- 1;
+step8/7;
