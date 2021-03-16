d7a79e1072dff640bf54497ebf1b7e7866ee855f
noahkimAC <kimnoah@hawaii.edu>
2021-03-15 14:36:57 -1000

    Turned it in on time, just cleaning up the format

--- Lab11/Ex6.js
+++ Lab11/Ex6.js
@@ -7,7 +7,6 @@ if(string_check < 0) errors.push('Negative value!');
 // Check if it is non-negative
 if(parseInt(string_check) != string_check) errors.push('Not an integer!'); 
 // Check that it is an integer
-    
 return returnErrors ? errors : (errors.length == 0);
 }
     
