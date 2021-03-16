bbfcf1c7cb0756ef35a3e7e43c023e368e41070b
noahkimAC <kimnoah@hawaii.edu>
2021-03-16 00:34:18 -1000

    Invoice4

---
+++ WODs/Invoice4/product_data.js
@@ -0,0 +1,30 @@
+// Product 1
+var product1 = {
+    "brand" : "HTC",
+    "price" : 40.00,
+    "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"
+},
+// Product 2
+var product2 = {
+    "brand" : "Apple",
+    "price" : 75.00,
+    "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"
+},
+// Product 3
+var product3 = {
+    "brand" : "Nokia",
+    "price" : 35.00,
+    "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"
+},
+// Product 4
+var product4 = {
+    "brand" : "Samsung",
+    "price" : 45.00,
+    "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"
+},
+// Product 5
+var product5 = {
+    "brand" : "Blackberry",
+    "price" : 10.00,
+    "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"
+}
