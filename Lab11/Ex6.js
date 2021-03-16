8c7fc7dbf6ea41a2d8d44278eef8aca7aae1db68
noahkimAC <kimnoah@hawaii.edu>
2021-03-15 19:06:54 -1000

    Turned it in on time, just cleaning up the formatting

--- Lab11/Ex6.js
+++ Lab11/Ex6.js
@@ -1,11 +1,11 @@
-function isNonNegInt(string_check, returnErrors = false) {
+function isNonNegInt(string_to_check, returnErrors = false) {
 errors = []; 
-// This function will return true if string_check is a NonNegInt. If returnErrors = true, it will return an array of reasons why it is a NonNegInt. 
-if(Number(string_check) != string_check) errors.push('Not a number!'); 
+// This function will return true if string_to_check is a NonNegInt. If returnErrors = true, it will return an array of reasons why it is a NonNegInt. 
+if(Number(string_to_check) != string_to_check) errors.push('Not a number!'); 
 // Check if string is a number value
-if(string_check < 0) errors.push('Negative value!'); 
+if(string_to_check < 0) errors.push('Negative value!'); 
 // Check if it is non-negative
-if(parseInt(string_check) != string_check) errors.push('Not an integer!'); 
+if(parseInt(string_to_check) != string_to_check) errors.push('Not an integer!'); 
 // Check that it is an integer
 return returnErrors ? errors : (errors.length == 0);
 }
